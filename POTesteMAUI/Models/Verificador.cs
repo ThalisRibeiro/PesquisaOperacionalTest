dc8df3671f7488ce6ec5c90d683445b5b6a5aacf
Thalis Ribeiro <thalis199@outlook.com>
2022-08-19 16:12:20 -0300

    Adicao do algoritimo que verifica qual o ponto otimo

---
+++ POTesteMAUI/Models/Verificador.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POTesteMAUI.Models
+{
+
+    public class Verificador
+    {
+        double ValorMaisAlto = 0;
+        int maiorI1;
+        int maiorI2;
+
+        public Verificador(Empresa empresa, ref Produto produto, ref Produto produto2)
+        {
+
+        }
+        //ele multiplica os valores, verifica se não ultrapassa as horas maximas e se foi o maior valor
+        void Comparador(Empresa empresa, ref Produto produto, ref Produto produto2)
+        {
+            int i1 = 0;
+            int i2 = 0;
+            do
+            {
+                do
+                {
+                    var hhTotais= produto.UsoHH *i1 +produto2.UsoHH * i2;
+                    var hmTotais = produto.UsoHm *i1 +produto2.UsoHm *i2;
+                    if (hhTotais > empresa.HHTotal || hmTotais > empresa.HMTotal)
+                        break;
+
+                    var valorTotal = produto.Valor * i1 + produto2.Valor *i2;
+                    if (valorTotal > ValorMaisAlto)
+                    {
+                        ValorMaisAlto = valorTotal;
+                        maiorI1 = i1;
+                        maiorI2 = i2;
+                    }
+                    i2 += 1;
+                } while (true);
+
+                if (i2 == 0)
+                    break;
+
+                i1++;
+                i2 = 0;
+
+            } while (true);
+            produto.Quantidade = maiorI1;
+            produto2.Quantidade = maiorI2;
+        }
+    }
+}
