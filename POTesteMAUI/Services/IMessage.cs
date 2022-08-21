cf545b3abc9d9259c22876e69e7f1cbc1137a8cf
Thalis Ribeiro <thalis199@outlook.com>
2022-08-21 19:13:18 -0300

    Criacao do servico de mensagens de display alert

---
+++ POTesteMAUI/Services/IMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POTesteMAUI.Services
+
+{
+    public interface IMessage
+    {
+        Task MostraMensagemErro(string title, string mensagem);
+    }
+}
---
+++ POTesteMAUI/Services/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POTesteMAUI.Services
+{
+    public class Message : IMessage
+    {
+        public async Task MostraMensagemErro(string title ,string mensagem)
+        {
+            await App.Current.MainPage.DisplayAlert("title", mensagem, "OK");
+        }
+    }
+}
