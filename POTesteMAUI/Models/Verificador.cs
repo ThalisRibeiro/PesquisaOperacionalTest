d6f0299f29b053501d462bac371e5163308718b1
Thalis Ribeiro <thalis199@outlook.com>
2022-08-21 19:15:24 -0300

    Adicao da testagem de erro que manda um display alert para pessoa informando

--- POTesteMAUI/Models/Verificador.cs
+++ POTesteMAUI/Models/Verificador.cs
@@ -3,34 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls.Shapes;
+using POTesteMAUI.Services;
 
 namespace POTesteMAUI.Models
 {
 
     public class Verificador
     {
+        readonly IMessage _message;
         public double ValorMaisAlto { get; private set; }
         //double ValorMaisAlto = 0;
         int maiorI1;
         int maiorI2;
+        int maiorI3;
+        bool deuerro;
 
         public Verificador(Empresa empresa, ref Produto produto, ref Produto produto2)
         {
+            //_message = DependencyService.Get<Services.IMessage>();
+            _message = DependencyService.Get<IMessage>();
             TesteErro(empresa,produto,produto2);
+            if (deuerro)
+                return;
             Comparador(empresa, ref produto, ref produto2);
         }
 
-        private void TesteErro(Empresa empresa,Produto produto, Produto produto1)
+        private void TesteErro(Empresa empresa,Produto produto, Produto produto1, Produto produto2 = null)
         {
-            if (empresa.MateriaPrima1 <1||empresa.MateriaPrima2<1)
+            if (produto2 ==null)
             {
-                //erro: quantidade de materia prima disponivel nao elegivel
-            }
-            if (produto.Valor <1||produto1.Valor<1||produto.UsoMateriaPrima1<1||produto1.UsoMateriaPrima1<1
-                ||produto.UsoMateriaPrima2<1||produto1.UsoMateriaPrima2<1)
-            {
-                //erro: valores dos produtos nao aceitos, verifique se todos sao maiores de 0
 
+                if (empresa.MateriaPrima1 < 1 || empresa.MateriaPrima2 < 1)
+                {
+                    deuerro = true;
+                    _message.MostraMensagemErro("ERRO", "quantidade de materia prima disponivel nao elegivel");
+                    return;
+                    //erro: quantidade de materia prima disponivel nao elegivel
+                }
+                if (produto.Valor < 1 || produto1.Valor < 1 || produto.UsoMateriaPrima1 < 1 || produto1.UsoMateriaPrima1 < 1
+                    || produto.UsoMateriaPrima2 < 1 || produto1.UsoMateriaPrima2 < 1)
+                {
+                    deuerro = true;
+                    _message.MostraMensagemErro("ERRO", "valores dos produtos nao aceitos, verifique se todos sao maiores de 0");
+                    return;
+                    //erro: valores dos produtos nao aceitos, verifique se todos sao maiores de 0
+
+                }
+            }
+            else
+            {
+
+                if (empresa.MateriaPrima1 < 1 || empresa.MateriaPrima2 < 1)
+                {
+                    deuerro = true;
+                    _message.MostraMensagemErro("ERRO", "quantidade de materia prima disponivel nao elegivel");
+                    return;
+                    //erro: quantidade de materia prima disponivel nao elegivel
+                }
+                if (produto.Valor < 1 || produto1.Valor < 1 || produto2.Valor<1 ||produto.UsoMateriaPrima1 < 1 || produto1.UsoMateriaPrima1 < 1
+                    || produto2.UsoMateriaPrima1<1 ||produto.UsoMateriaPrima2 < 1 || produto1.UsoMateriaPrima2 < 1 ||produto2.UsoMateriaPrima2<1)
+                {
+                    deuerro = true;
+                    _message.MostraMensagemErro("ERRO", "valores dos produtos nao aceitos, verifique se todos sao maiores de 0");
+                    return;
+                    //erro: valores dos produtos nao aceitos, verifique se todos sao maiores de 0
+
+                }
             }
         }
 
@@ -68,5 +107,6 @@ namespace POTesteMAUI.Models
             produto.Quantidade = maiorI1;
             produto2.Quantidade = maiorI2;
         }
+       
     }
 }
