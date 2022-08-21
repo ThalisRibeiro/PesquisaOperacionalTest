78b1bb790ab72c5f285c8aa9a6d16763ad319539
Thalis Ribeiro <thalis199@outlook.com>
2022-08-21 17:04:33 -0300

    Exclusao da quantidade no ctor

--- POTesteMAUI/Models/Produto.cs
+++ POTesteMAUI/Models/Produto.cs
@@ -14,11 +14,10 @@ namespace POTesteMAUI.Models
         public int UsoMateriaPrima1 { get; set; }
         public int UsoMateriaPrima2 { get; set; }
 
-        public Produto(string nome, double valor, int quantidade, int usoMP1, int usoMP2)
+        public Produto(string nome, double valor, int usoMP1, int usoMP2)
         {
             Nome = nome;
             Valor = valor;
-            Quantidade = quantidade;
             UsoMateriaPrima1 = usoMP1;
             UsoMateriaPrima2 = usoMP2;
         }
