5306bb22b05dcd3c685b6e624a12360d374e3cd5
Thalis Ribeiro <thalis199@outlook.com>
2022-08-21 19:26:13 -0300

    Forcando a mostrar os placeholders

--- POTesteMAUI/Views/MainPage.xaml.cs
+++ POTesteMAUI/Views/MainPage.xaml.cs
@@ -7,7 +7,15 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+		this.SegundaMateriaPrimaQtd.Text="";
+		this.PrimeiraMateriaPrimaQtd.Text = "";
+		this.PrimeiroProdutoValor.Text = "";
+		this.SegundoProdutoValor.Text = "";
+		this.PrimeiroProdutoMP1.Text = "";
+		this.SegundoProdutoMP1.Text = "";
+		this.PrimeiroProdutoMP2.Text = "";
+		this.SegundoProdutoMP2.Text = "";
+    }
 
 
 }
