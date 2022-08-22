87e9785f88173d76c9680fcde42a9215a8135bc3
Thalis Ribeiro <thalis199@outlook.com>
2022-08-22 20:10:29 -0300

    Criacao da viewmodel que vai mostar os valores

---
+++ POTesteMAUI/ViewModels/ResultPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using POTesteMAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POTesteMAUI.ViewModels
+{
+    [QueryProperty(nameof(Produtos), nameof(Produtos))]
+    [QueryProperty(nameof(Empresa), nameof(Empresa))]
+    [INotifyPropertyChanged]
+    public partial class ResultPageViewModel
+    {
+        [ObservableProperty]
+        ObservableCollection<Produto> produtos;
+        [ObservableProperty]
+        Empresa empresa;
+        [ObservableProperty]
+        double lucroTotal;
+
+        public ResultPageViewModel()
+        {
+           
+        }
+
+        [RelayCommand]
+        void CalculcaLucros()
+        {
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                LucroTotal += produtos[i].Lucro;
+            }
+        }
+    }
+}
