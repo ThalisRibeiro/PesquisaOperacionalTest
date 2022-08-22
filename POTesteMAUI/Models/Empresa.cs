616ab345d17cd401b8d6b77d17a513b07a5f9c4c
Thalis Ribeiro <thalis199@outlook.com>
2022-08-22 20:06:28 -0300

    Adicao de uma prop pra mostrar o lucro total da empresa e quanto foi usado de materia prima

--- POTesteMAUI/Models/Empresa.cs
+++ POTesteMAUI/Models/Empresa.cs
@@ -12,6 +12,9 @@ namespace POTesteMAUI.Models
         public int MateriaPrima2 { get; set; }
         public string NomeMateriaPrima1 { get; set; }
         public string NomeMateriaPrima2 { get; set; }
+        public int MP1Usada { get; set; }
+        public int MP2Usada { get; set; }
+        public double LucroToral { get; set; }
         public Empresa(int mp1Disponivel, int mp2Disponivel, string nMP1,string nMP2)
         {
             MateriaPrima1 = mp1Disponivel;
